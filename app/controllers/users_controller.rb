f7dc7591bb4989eeaaf653b938935df623893252
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-24 13:59:25 -0700

    working user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,31 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def show
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+	end
+
+	def create
+		@user = User.create(user_params)
+    	redirect_to root_path
+	end
+
+	def udpate
+	end
+
+	def destroy
+	end
+
+	private
+	def user_params
+		#related to form post data in create
+		params.require(:user).permit(:first_name, :last_name, :email, :password)
+	end
+
 end
